c1e673b472e265043e011d0070ac66240d95ce30
James Noeckel <jamesn8@cs.washington.edu>
2021-06-25 14:12:47 -0700

    added tests

--- test_physics.py
+++ test_physics.py
@@ -1,5 +1,3 @@
-#In[0]:
-
 from meshplot import plot, interact
 import ipywidgets
 import numpy as np
@@ -22,17 +20,15 @@ def display_animation(v1, f1, v2, f2, states):
         vs = []
         for j in range(2):
             v = (v1, v2)[j]
-            R = vm.quaternion_to_matrix(states[t][j][0])
-            x = states[t][j][1]
+            R = vm.quaternion_to_matrix(states[t][1][j][0])
+            x = states[t][1][j][1]
             newv = (R @ v.T).T + x
             vs.append(newv)
         fullv = np.vstack(vs)
         p.update_object(vertices=fullv)
 
 
-#In[1]:
 # ### off-center collision
-
 v, f = igl.read_triangle_mesh('data/cylinder3.obj')
 grid_len = 0.02
 density = 1
@@ -43,5 +39,4 @@ obj2 = phys.PhysObject(v, f, grid_len, translation=np.array([0.5, 2.1, 0], np.fl
 
 sim = phys.Simulator(obj1, obj2, np.array([-1.1, -1.1, -1.1]), np.array([1.1, 3.1, 1.1]), 0.02, 100, 0)
 sim.integrate(0.001, 1000)
-print(sim.allstates)
-#display_animation(v, f, v, f, sim.allstates)
+display_animation(v, f, v, f, sim.allstates)
