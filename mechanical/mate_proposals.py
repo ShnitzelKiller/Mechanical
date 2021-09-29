13376b4afcfc124dae0cc4f10565c09e44628066
James Noeckel <jamesn8@cs.washington.edu>
2021-09-28 18:07:40 -0700

    equivalent frames for mate proposals

--- mechanical/mate_proposals.py
+++ mechanical/mate_proposals.py
@@ -4,6 +4,8 @@ import numpy as np
 import numpy.linalg as LA
 import numba
 from numba import njit
+from scipy.spatial.transform import Rotation as R
+
 
 def cluster_axes(nnhash, axes):
     visited = set()
@@ -27,9 +29,44 @@ def homogenize_sign(vec):
             maxabs = vecabs
             maxdim = i
     if vec[maxdim] < 0:
-        return -vec
+        return -vec, True
     else:
-        return vec
+        return vec, False
+
+@njit
+def vec3_tuple(vec):
+    return (vec[0],vec[1],vec[2])
+
+@njit
+def tuple_vec3(vec,tup):
+    vec[0] = tup[0]
+    vec[1] = tup[1]
+    vec[2] = tup[2]
+    return vec
+
+@njit
+def homogenize_frame(frame, z_flip_only=True):
+    #homogenized x and y can only differ by a swap if the z axes of two orthogonally rotated frames differ only by sign
+    x, _ = homogenize_sign(frame[:,0])
+    y, _ = homogenize_sign(frame[:,1])
+    z, _ = homogenize_sign(frame[:,2])
+
+    if z_flip_only:
+        vectors = sorted([vec3_tuple(x),vec3_tuple(y)])
+        z_final = z
+        x_final = tuple_vec3(x,vectors[0])
+    else:
+        vectors = sorted([vec3_tuple(x),vec3_tuple(y),vec3_tuple(z)])
+        z_final = tuple_vec3(z,vectors[0])
+        x_final = tuple_vec3(x,vectors[1])
+
+    y_final = np.cross(z_final,x_final)
+
+    frame_out = np.empty_like(frame)
+    frame_out[:,0] = x_final
+    frame_out[:,1] = y_final
+    frame_out[:,2] = z_final
+    return frame_out
 
 def mate_proposals(parts, epsilon_rel=0.001, max_groups=10):
     """
@@ -39,8 +76,7 @@ def mate_proposals(parts, epsilon_rel=0.001, max_groups=10):
 
     #build initial data structures and nearest neighbor hashmap for axial directions
     maxdim = max([(part.bounding_box()[1]-part.bounding_box()[0]).max() for _, part in parts])
-    mc_axis = []
-    mc_plane = []
+    mc_frames = []
     mc_origin = []
     interval2part = []
     part2offset = dict()
@@ -51,36 +87,45 @@ def mate_proposals(parts, epsilon_rel=0.001, max_groups=10):
             cs = mc.get_coordinate_system()
             origin = tf[:3,:3] @ cs[:3,3] + tf[:3,3]
             frame = tf[:3,:3] @ cs[:3,:3]
-            z_axis = frame[:,2]
-            xy_plane = frame[:,:2]
-            mc_axis.append(homogenize_sign(z_axis))
-            mc_plane.append(xy_plane)
+            
+            frame = homogenize_frame(frame, z_flip_only=True)
+
+            mc_frames.append(frame)
             mc_origin.append(origin)
         new_total = total_mcs + len(part.all_mate_connectors)
         interval2part.append((total_mcs, new_total, i))
         part2offset[i] = total_mcs
         total_mcs = new_total
+    mc_axis = [frame[:,2] for frame in mc_frames]
+    mc_quat = [R.from_matrix(frame).as_quat() for frame in mc_frames]
     nnhash = pspart.NNHash(mc_axis, 3, epsilon_rel)
     tree = IntervalTree([Interval(l, u, d) for l, u, d in interval2part])
     
     #for each axial cluster, find nearest neighbors of projected set of frames with the same axis direction
     proposals = set()
     result = cluster_axes(nnhash, mc_axis)
+    # print(result)
     group_indices = list(result)
     group_indices.sort(key=lambda k: result[k], reverse=True)
     for ind in group_indices[:max_groups]:
-        #print('processing group',ind,':',result[ind])
+        # print('processing group',ind,':',result[ind])
         proj_dir = mc_axis[ind]
         same_dir_inds = list(nnhash.get_nearest_points(proj_dir))
-        xy_plane = mc_plane[ind]
-        #print('projecting')
-        projected_origins = list((np.array([mc_origin[i] for i in same_dir_inds]) @ xy_plane)/maxdim)
-        #print('hashing')
-        proj_hash = pspart.NNHash(projected_origins, 2, epsilon_rel)
-        #print('querying')
-        for igroup,loc in enumerate(projected_origins):
+        xy_plane = mc_frames[ind][:,:2]
+        # print('projecting')
+        subset_stacked = np.array([mc_origin[i] for i in same_dir_inds])
+        projected_origins = list((subset_stacked @ xy_plane)/maxdim)
+        projected_z = list((subset_stacked @ proj_dir)/maxdim)
+        z_quat = [np.concatenate([mc_quat[same_dir_inds[i]], [projected_z[i]]]) for i in range(len(same_dir_inds))]
+        # print('hashing')
+        coplanar_hash = pspart.NNHash(z_quat, 5, epsilon_rel)
+        coaxial_hash = pspart.NNHash(projected_origins, 2, epsilon_rel)
+        # print('querying')
+        for igroup in range(len(same_dir_inds)):
+            nearest_coaxial = coaxial_hash.get_nearest_points(projected_origins[igroup])
+            nearest_coplanar = coplanar_hash.get_nearest_points(z_quat[igroup])
+            nearest = list(nearest_coaxial.union(nearest_coplanar))
             i = same_dir_inds[igroup]
-            nearest = list(proj_hash.get_nearest_points(loc))
             part_index = next(iter(tree[i])).data
             for jgroup in nearest:
                 j = same_dir_inds[jgroup]
