save normalization transforms and use them to fix visualization of augmented parts (still need to use to fix penetration)

mechanical/data/assembly_data.py
@@ -148,7 +148,7 @@ class AssemblyInfo:
     def transform_parts(self):
         p_normalized = np.identity(4, dtype=float)
         p_normalized[:3,3] = -self.median
-        p_normalized[3,3] = self.maxdim #todo: figure out if this is double the factor
+        p_normalized[3,3] = self.maxdim
 
         norm_matrices = [p_normalized @ tf for tf in self.occ_transforms]
         transformed_parts = []
@@ -183,6 +183,7 @@ class AssemblyInfo:
         #these may change if we choose to handle transforms differently. TODO if changing: also handle the epsilons/scaling thresholds!
         self.mate_transforms = norm_matrices
         self.part_transforms = [np.identity(4) for part in self.parts]
+        self.norm_transform = p_normalized
 
 
     def precompute_geometry(self):

mechanical/utils/data.py
@@ -2,6 +2,7 @@ from mechanical.onshape import Mate
 from enum import Enum
 import numpy as np
 from mechanical.utils.transforms import compute_basis
+import numpy.linalg as LA
 
 def df_to_mates(mate_subset):
     mates = []
@@ -15,14 +16,20 @@ def df_to_mates(mate_subset):
         mates.append(mate)
     return mates
 
-def newmate_df_to_mates(newmate_subset, batch):
+def newmate_df_to_mates(newmate_subset, batch, norm_tf):
+    inv_tf = LA.inv(norm_tf)
     mates = []
     for j in range(newmate_subset.shape[0]):
         if newmate_subset.iloc[j]['added_mate']:
             newmate_row = newmate_subset.iloc[j]
             axis_index = newmate_subset.iloc[j]['axis_index']
             origin = batch.mcfs[axis_index,3:].numpy()
+            origin_homo = np.concatenate([origin, [1]])
+            origin_homo = inv_tf @ origin_homo
+            origin_homo /= origin_homo[3]
+            origin = origin_homo[:3]
             axis = batch.mcfs[axis_index,:3].numpy()
+            axis = inv_tf[:3,:3] @ axis
             axes = compute_basis(axis)
             axes = np.concatenate([axes.T, axis[:,np.newaxis]], axis=1)
             mate = Mate(occIds=[newmate_row[f'part{p+1}'] for p in range(2)],

mechanical/visualize2.py
@@ -109,6 +109,7 @@ def plot_assembly(geo, mates, rigid_labels=None, view_width=800, view_height=600
     maxDim = (maxPt - minPt).max()
     scalemat = np.identity(4)
     scalemat[:3,:3] /= maxDim
+    scalemat[:3,3] = -meanPt
     tfs = {occ: scalemat @ geo[occ][0] for occ in geo}
     mesh_data = {}
     for i in geo:
@@ -176,12 +177,13 @@ def plot_assembly(geo, mates, rigid_labels=None, view_width=800, view_height=600
             for mated in mate.matedEntities:
                 if i < no_mate_transform_after_index or no_mate_transform_after_index < 0:
                     tf = tfs[mated[0]]
-                    newaxes = tf[:3, :3] @ mated[1][1]
-                    newaxes *= maxDim
-                    neworigin = tf[:3,:3] @ mated[1][0] + tf[:3,3]
                 else:
-                    newaxes = mated[1][1]
-                    neworigin = mated[1][0]
+                    tf = scalemat
+                
+                newaxes = tf[:3, :3] @ mated[1][1]
+                newaxes *= maxDim
+                neworigin = tf[:3,:3] @ mated[1][0] + tf[:3,3]
+                
                 mate_objects = add_axis(neworigin, newaxes[:,0], newaxes[:,1], newaxes[:,2], scale=0.1, mate_type=mate.type)
                 all_mate_objects += mate_objects
 