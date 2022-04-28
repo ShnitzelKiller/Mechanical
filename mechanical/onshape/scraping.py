f9899eb0c50df92f97b3f2310e17f9615d786b63
James Noeckel <jamesn8@cs.washington.edu>
2022-04-28 13:38:18 -0700

    first pass at assembly duplicator that deletes mates

--- mechanical/onshape/scraping.py
+++ mechanical/onshape/scraping.py
@@ -8,7 +8,7 @@ class AssemblyDuplicator:
         self.api = Onshape()
     
 
-    def process_assembly(self, did, mv, eid):
+    def process_assembly(self, did, mv, eid, newdid=None, newwid=None):
 
         data_path = self.data_dir / 'assemblies' / did / mv / eid / 'default.json'
         document_path = self.data_dir / 'documents' / did / f'{mv}.json'
@@ -20,25 +20,79 @@ class AssemblyDuplicator:
                 document_definition = json.load(f)
             
             wid = document_definition['data']['defaultWorkspace']['id']
-            newname = f'{document_definition["data"]["name"]} Mateless'
+
+            elements_response = self.get_elements(did,wid)
+
+            element_name = None
+            for element in elements_response:
+                if element['id'] == eid:
+                    element_name = element['name']
+                    break
             
-            endpoint = f'documents/{did}/workspaces/{wid}/copy'
-            query = {'isPublic':True, 'newName': newname}
+            if element_name is None:
+                raise Exception(f'element not in document')
 
-            response = self.api.request('post', f'/api/{endpoint}', body=query)
-            if response.status_code < 200 or response.status_code >= 300:
-                raise Exception(f'Request for assembly features {did}/{mv}/{eid} was not successful.')
-            json_data = response.json()
-            return json_data
+            if newdid is None:
+                newname = f'{document_definition["data"]["name"]} Mateless'
+                
+                endpoint = f'documents/{did}/workspaces/{wid}/copy'
+                query = {'isPublic':True, 'newName': newname}
 
+                response = self.api.request('post', f'/api/{endpoint}', body=query)
+                if response.status_code < 200 or response.status_code >= 300:
+                    raise Exception(f'duplicating assembly was not successful.')
+                
+                
+                new_assembly_json = response.json()
+                newdid = new_assembly_json['newDocumentId']
+                newwid = new_assembly_json['newWorkspaceId']
+
+            new_elements_response = self.get_elements(newdid, newwid)
+            neweid = None
+            for element in new_elements_response:
+                if element['name'] == element_name:
+                    neweid = element['id']
+                    break
+            if neweid is None:
+                raise Exception(f'element not copied with the same name???')
+            
+            features = self.get_features(newdid, newwid, neweid)
+            for feature in features['features']:
+                if feature['message']['featureType'] == 'mate':
+                    fid = feature['message']['featureId']
+                    self.delete_feature(newdid, newwid, neweid, fid)
+        
+            return newdid, newwid, neweid
+
+            
         else:
             raise ValueError
         
+    
+    def get_elements(self, did, wid):
+        endpoint = f'documents/d/{did}/w/{wid}/elements'
+        response = self.api.request('get', f'/api/{endpoint}')
+        if response.status_code < 200 or response.status_code >= 300:
+            raise Exception(f'requesting elements was not successful.')
+        return response.json()
+    
+    def get_features(self, did, wid, eid):
+        endpoint = f'assemblies/d/{did}/w/{wid}/e/{eid}/features'
+        response = self.api.request('get',f'/api/{endpoint}')
+        if response.status_code < 200 or response.status_code >= 300:
+            raise Exception(f'requesting features was not successful.')
+        return response.json()
+    
+    def delete_feature(self, did, wid, eid, fid):
+        endpoint = f'assemblies/d/{did}/w/{wid}/e/{eid}/features/featureid/{fid}'
+        response = self.api.request('delete',f'/api/{endpoint}')
+        if response.status_code < 200 or response.status_code >= 300:
+            raise Exception(f'deleting feature d/{did}/w/{wid}/e/{eid}/f/{fid} was not successful.')
 
 if __name__ == '__main__':
 
     dup = AssemblyDuplicator('/projects/grail/benjones/cadlab/data')
 
-    response = dup.process_assembly('d8a303e6739db91d4aab5bc5','150b669cde452a8c0eceeda0','6a06a9a7db2c8b85d3601940')
+    newdid, newwid, neweid = dup.process_assembly('d8a303e6739db91d4aab5bc5','150b669cde452a8c0eceeda0','6a06a9a7db2c8b85d3601940', newdid='63dd6441d8c1932d211ba4a1', newwid='eaea8bfef788195bd549952b')
 
-    print(response)
+    print(newdid, newwid, neweid)
